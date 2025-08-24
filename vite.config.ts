d19404d4c01f77ae9766a99b88b8d4a3d5077921
redaor <reda_sahraoui@outlook.fr>
2025-08-24 08:35:41 +0200

    Add security headers for Zoom

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 important : indique que les assets doivent être cherchés relativement
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
