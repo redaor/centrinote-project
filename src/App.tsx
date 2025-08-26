6503b063e946af966eaa2960f3aed32484ec69a4
redaor <reda_sahraoui@outlook.fr>
2025-08-26 12:07:52 +0200

    Fix OAuth state issue - remove conflicting callback files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React, { useEffect } from 'react';
 import { AppRouter } from './components/routing/AppRouter';
-// @ts-ignore - Composant de test temporaire
-import ZoomTest from './components/ZoomTest.jsx';
 
 function App() {
   return (
     <div className="App">
       <AppRouter />
-      
-      {/* Composant de test temporaire */}
-      <ZoomTest />
     </div>
   );
 }
